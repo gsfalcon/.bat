ead7be5ce26428e3c74975bab8b5a1c26c256ea8
gsFALCON <gsfalcon@proton.me>
2023-02-01 12:57:55 -0300

    Update shutdownWin.bat

--- shutdownWin.bat
+++ shutdownWin.bat
@@ -3,16 +3,19 @@
 title SHUTDOWN - DESLIGAMENTO AUTOMATICO
 cls
 color c0
-
+echo.
 echo.
 echo.             S H U T D O W N
 echo.  ____________________________________________________________________________
 echo. /___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/
 echo.
-
-set /p horas=Digite a quantidade de horas até o desligamento: 
+echo.
+set /p horas=Digite a quantidade de horas ate o desligamento: 
+echo.
 set /a segundos=%horas% * 60 * 60
+echo.
 shutdown -s -t %segundos%
+echo.
 echo Desligamento agendado para daqui a %horas% hora(s).
-
+echo.
 pause
